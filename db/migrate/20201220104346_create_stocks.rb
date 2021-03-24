d714f79a7501c1336374b093dbf7530186bd8e80
leoggonzalez <leoggonzalez@hotmail.com>
2021-03-24 21:04:22 +0100

    added ajax call

--- db/migrate/20201220104346_create_stocks.rb
+++ db/migrate/20201220104346_create_stocks.rb
@@ -9,5 +9,3 @@ class CreateStocks < ActiveRecord::Migration[6.0]
     end
   end
 end
-
-client = IEX::Api::Client.new(publishable_token: 'pk_793a06f9973541a0814ab9cbcc6702b5', secret_token: 'sk_e5493d46ba0245d2a25aa73d1e4cf99e', endpoint: 'https://cloud.iexapis.com/v1')
